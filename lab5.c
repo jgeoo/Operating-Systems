27067edac4fd02aec149a61032acd7de54a2a7f2
jgeoo <jgeoo@Ubuntu.myguest.virtualbox.org>
2023-05-02 12:55:06 +0300

    Script noerr/war with no line

--- lab5.c
+++ lab5.c
@@ -5,135 +5,156 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> 
+
 void gotoformat(struct stat file){
     printf("USER:\n");
                 if(file.st_mode & S_IRUSR )
-                {
-                    printf("Read:YES\n");
-                }
+                    {
+                         printf("Read:YES\n");
+                    }
                 else{
-                    printf("Read:NO\n");
-                }
+                         printf("Read:NO\n");
+                    }
                 if(file.st_mode & S_IWUSR )
-                {
-                    printf("Write:YES\n");
-                }
+                    {
+                         printf("Write:YES\n");
+                    }
                 else{
-                    printf("Write:NO\n");
-                }
+                        printf("Write:NO\n");
+                    }
                 if(file.st_mode & S_IXUSR )
-                {
-                    printf("Execution:YES\n");
-                }
+                    {
+                        printf("Execution:YES\n");
+                    }
                 else{
-                    printf("Execution:NO\n");
-                }
+                        printf("Execution:NO\n");
+                    }
     printf("\nGROUP:\n");
                 if(file.st_mode & S_IRGRP )
-                {
-                    printf("Read:YES\n");
-                }
+                    {
+                        printf("Read:YES\n");
+                    }   
                 else{
-                    printf("Read:NO\n");
-                }
+                        printf("Read:NO\n");
+                    }
                 if(file.st_mode & S_IWGRP )
-                {
-                    printf("Write:YES\n");
-                }
+                    {
+                        printf("Write:YES\n");
+                    }
                 else{
-                    printf("Write:NO\n");
-                }
+                        printf("Write:NO\n");
+                    }
                 if(file.st_mode & S_IXGRP )
-                {
-                    printf("Execution:YES\n");
-                }
+                    {
+                        printf("Execution:YES\n");
+                    }
                 else{
-                    printf("Execution:NO\n");
-                }
+                        printf("Execution:NO\n");
+                    }
     printf("\nOTHERS:\n");
                 if(file.st_mode &  S_IROTH)
-                {
-                    printf("Read:YES\n");
-                }
+                    {   
+                        printf("Read:YES\n");
+                    }
                 else{
-                    printf("Read:NO\n");
-                }
+                        printf("Read:NO\n");
+                    }
                 if(file.st_mode &   S_IWOTH )
-                {
-                    printf("Write:YES\n");
-                }
+                    {
+                        printf("Write:YES\n");
+                    }
                 else{
-                    printf("Write:NO\n");
-                }
+                        printf("Write:NO\n");
+                    }
                 if(file.st_mode & S_IXOTH )
-                {
-                    printf("Execution:YES\n");
-                }
+                    {
+                        printf("Execution:YES\n");
+                    }
                 else{
-                    printf("Execution:NO\n");
-                }
+                        printf("Execution:NO\n");
+                    }
 }
 void getLinkname(char* file){
+
+    char buffer[1024];
+    readlink(file,buffer,sizeof(buffer)-1);
+
+    printf("The link name of %s is %s\n ",file,buffer);
+
+}
+void getLinksize(char* file){
+
     char buffer[1024];
 
     readlink(file,buffer,sizeof(buffer)-1);
-    printf("The link name of %s is %s\n ",file,buffer);
-}
-void getLinksize(char* file){
-    char buffer[1024];
-    readlink(file,buffer,sizeof(buffer)-1);
+
     struct stat Target;
     lstat(buffer,&Target);
+
     printf("The link size of %s is %ld\n ",file,Target.st_size);
+
 }
 void check_c_files(char* dir_name){
+
     DIR *dir;
     struct dirent *ent;
     int count = 0;
     int len;
     dir = opendir(dir_name);
+
     if(dir!=NULL )
     {
         while((ent = readdir(dir)) !=NULL){
             len = strlen(ent->d_name);
-            if(strcmp(ent->d_name+len -2,".c") == 0)
-                count++;
+                if(strcmp(ent->d_name+len -2,".c") == 0)
+                    count++;
 
         }
         closedir(dir);
     }else{
         printf("Error opening dir\n");
     }
+
     printf("The number of c files is %d\n",count);
 
 }
-int check_c_files_regularfile(char* file_name){
+int check_c_files_regularfile(char* file_name){ 
+
     int len = strlen(file_name);
-   
             if(strcmp(file_name+len -2,".c") == 0)
-                return 1;
+                    return 1;
+
     return 0;
 
 }
 int main(int argc, char *argv[]){
+
     struct stat file_stat;
     pid_t pid_switch, process_forkfile;
+
     if(argc == 1){
+
         printf("Not multiple cmd agr");
         return EXIT_FAILURE;
+
     }else{
+
     for( int  i = 1; i < argc ;i++){
+
     lstat(argv[i],&file_stat);
+
     if(S_ISREG(file_stat.st_mode))
     {
         char c;
         char link[24];
         struct tm *time = localtime(&file_stat.st_mtime);
+
         printf("The file  ' %s ' is a regular file",argv[i]);
         printf("\nA) Regular file\n -n (file name) \n -d (dim/size) \n -h (number of hard links \n -m (time of last modif) \n -a (acces rights) \n -l (create a symbolic link)\n\n");
         
         if(check_c_files_regularfile(argv[i]) == 1){
+
         process_forkfile = fork();
         if(process_forkfile< 0 ){
             perror("Process for regular file didn t start");
@@ -141,13 +162,16 @@ int main(int argc, char *argv[]){
             execlp("./script.sh","./script.sh",argv[i],NULL);
              exit(EXIT_FAILURE);
         }
+
         }
        
         scanf(" %c",&c);
         pid_switch = fork();
+
         if(pid_switch < 0){
             perror("Didn t start");
         }else if(pid_switch == 0){
+
         switch (c)
         {
         case 'n':printf("File name:%s\n",argv[i]);break;
@@ -166,16 +190,20 @@ int main(int argc, char *argv[]){
             break;
         
         }
+
         exit(EXIT_FAILURE);
         }
     }
 
     else if(S_ISLNK(file_stat.st_mode)){
+
         printf("The file ' %s ' a symbolic link",argv[i]);
         char c;
+
         printf("\n -n Link name \n-l delete link \n -d Size of link \n-z size of target \n-a acces rights \n");
         scanf(" %c",&c);
     
+
         switch (c)
         {
             case'n':getLinkname(argv[i]);break;
@@ -185,8 +213,8 @@ int main(int argc, char *argv[]){
             case'z':getLinksize(argv[i]);break;
             case'a':gotoformat(file_stat);break;
             default:
-                printf("Not a case for symbolic file");
-                break;
+                    printf("Not a case for symbolic file");
+                    break;
             
         }
     }
@@ -197,18 +225,18 @@ int main(int argc, char *argv[]){
         scanf(" %c",&c);
     
         switch(c)
-        {
+            {
             case'n':printf("File name is %s\n",argv[i]);break;
             case'd':printf("Size of the file is %ld\n",file_stat.st_size);break;
             case'c':check_c_files(argv[i]);break;
             case'a':gotoformat(file_stat);break;
             default:
-                printf("Not a case for a Directory");
-                break;
+                    printf("Not a case for a Directory");
+                    break;
                 
             
+            }
         }
-    }
     }
     }
 
--- script.sh
+++ script.sh
@@ -3,14 +3,14 @@ score=0
 checkc=0
 if test -f $1
 then 
-checkc=`$2|grep \.c$|wc -l`
-if test checkc != 0 
+if echo "$1" | egrep '\.c$' >/dev/null
 then
 
 score=`gcc -Wall "$1" 2>&1| egrep 'error:|warning:' | wc -l`
 echo "For the file $1 there is $score warnings/errors"
 
 else echo "Is not a C file"
+echo `wc -l $1`
 fi
 else echo "Not a Directory"
 fi
